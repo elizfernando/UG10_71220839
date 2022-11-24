84f37f0e3c5c503e98dedb3b9a36b6846afbf56d
elizfernando <118380082+elizfernando@users.noreply.github.com>
2022-11-24 13:09:54 +0700

    Add files via upload

---
+++ 1_B_71220839.py
@@ -0,0 +1,23 @@
+print ("~~~~~~~~~~ /('v')\ ~~~~~~~~~~")
+print ("PROGRAM PENGHITUNG VOLUME BANGUN RUANG")
+print ("~~~~~~~~~~ \('v')/ ~~~~~~~~~~")
+print ("Pilihlah salah satu bangung ruang yang ingin dihitung volumenya:")
+print ("1. Limas")
+print ("2. Bola")
+print ("3. Prisma")
+print ("4. Kerucut")
+x = int(input ("Masukkan pilihan Anda: "))
+if x == 1 :
+    y = int(input ("Masukkan panjang sisi alas limas: "))
+    z = int(input ("Masukkan tinggi limas: "))
+    limas = (1/3*y*y*z)
+    print ("Volume limas tersebut adalah",limas)
+elif x == 2 :
+    xy = ("Masukkan jari jari bola:")
+    bola = ((4/3)*3.14*xy*xy*xy)
+    print ("Volume Bola tersebut adalah",bola)
+elif x == 4 :
+    xz = ("Masukkan jari jari kerucut:")
+    yz = ("Masukkan tinggi kerucut:")
+    kerucut = (1/3*3.14*xz*xz*yz)
+    print ("Volume Kerucut tersebut",kerucut)
